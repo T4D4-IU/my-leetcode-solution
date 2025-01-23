ea241915432ce0625c24d3a714e67b0e7b98417f
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2025-01-23 21:58:15 +0900

    Time: 0 ms (100.00%) | Memory: 2.3 MB (10.74%) - LeetSync

--- 9-palindrome-number/palindrome-number.rs
+++ 9-palindrome-number/palindrome-number.rs
@@ -2,15 +2,6 @@ impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         let x_str = x.to_string();
 
-        let x_len = x_str.len();
-
-        let x_chars: Vec<char> = x_str.chars().collect();
-
-        for i in 0..x_len / 2 {
-            if x_chars[i] != x_chars[x_len - 1 -i] {
-                return false;
-            }
-        } 
-        true
+        x_str == x_str.chars().rev().collect::<String>()
     }
 }
