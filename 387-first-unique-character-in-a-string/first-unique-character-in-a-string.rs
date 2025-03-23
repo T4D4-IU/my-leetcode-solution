0de58850fc5ae58b7aae6f49ec42c81fbaeb999a
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2025-03-23 23:42:44 +0900

    Time: 9 ms (53.77%) | Memory: 2.3 MB (98.11%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
@@ -1,18 +1,21 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn first_uniq_char(s: String) -> i32 {
-        use std::collections::HashMap;
         let mut counts = HashMap::new();
 
+        // 文字の出現回数をカウント
         for c in s.chars() {
             *counts.entry(c).or_insert(0) += 1;
         }
+
+        // 最初の一度だけ登場する文字のインデックスを探す
         for (i, c) in s.chars().enumerate() {
-            if let Some(&count) = counts.get(&c) {
-                if count == 1 {
-                    return i as i32;
-                }
+            if counts[&c] == 1 {
+                return i as i32;
             }
         }
+
         -1
     }
 }
