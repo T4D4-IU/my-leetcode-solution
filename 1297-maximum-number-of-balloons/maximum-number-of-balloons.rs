54ddd2adf8f31e8fd780b9dc6dfd0248fab7334f
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2025-05-27 16:48:40 +0900

    Time: 0 ms (100.00%) | Memory: 2.1 MB (68.18%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.rs
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.rs
@@ -1,40 +1,31 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn max_number_of_balloons(text: String) -> i32 {
-        use std::collections::HashMap;
-        let mut char_counts: HashMap<char, i32> = HashMap::new();
-        
-        // "balloon"を構成する文字列のみをカウント対象とする
-        let target_chars = ['b', 'a', 'l', 'o', 'n'];
-
-        // HashMapに初期値を入れる
-        char_counts.insert('b', 0);
-        char_counts.insert('a', 0);
-        char_counts.insert('l', 0);
-        char_counts.insert('o', 0);
-        char_counts.insert('n', 0);
-
-        for c in text.chars() {
-            if target_chars.contains(&c) {
-                *char_counts.entry(c).or_insert(0) += 1;
-            }
+        // 1. text 内の各文字の出現回数をカウントする
+        let mut text_counts = HashMap::new();
+        for char_c in text.chars() {
+            *text_counts.entry(char_c).or_insert(0) += 1;
         }
 
-        // lと0は２回使うので半分にする。
-        char_counts.entry('l')
-            .and_modify(|count| {
-                *count /= 2
-            });
-        char_counts.entry('o')
-            .and_modify(|count| {
-                *count /= 2
-            });
-        
+        // 2. "balloon" を作るのに必要な文字の出現回数をカウントする
+        //    実際には固定なので、直接値を使います。
+        //    b: 1, a: 1, l: 2, o: 2, n: 1
 
-        // 一番数が少ない文字の数だけballonを作れる
-        if let Some(min) = char_counts.values().min() {
-            *min
-        } else {
-            0
-        }
+        // 3. 各文字で作れる "balloon" の数を計算する
+        let count_b = *text_counts.get(&'b').unwrap_or(&0);
+        let count_a = *text_counts.get(&'a').unwrap_or(&0);
+        let count_l = *text_counts.get(&'l').unwrap_or(&0) / 2; // 'l' は2つ必要
+        let count_o = *text_counts.get(&'o').unwrap_or(&0) / 2; // 'o' は2つ必要
+        let count_n = *text_counts.get(&'n').unwrap_or(&0);
+
+        // 4. これらのうち最小のものが作れる "balloon" の最大数
+        let mut result = count_b;
+        result = result.min(count_a);
+        result = result.min(count_l);
+        result = result.min(count_o);
+        result = result.min(count_n);
+
+        result
     }
 }
